10c221e31b8bcd575a0200054c2396b0cb01c903
risson <risson.yao@gmail.com>
2013-08-13 14:41:48 +0800

    Create sidebar.js

---
+++ sidebar.js
@@ -0,0 +1,59 @@
+#aliasSidebar {
+	position:fixed;
+	top:0px;
+	right:0px;
+	width:20%;
+	height:100%;
+	background:#f6ebbb;
+	padding: 10px;
+	border:1px solid black;
+	font: 11px arial,sans-serif;
+	overflow-y: scroll;
+	z-index:999999;
+}
+#file_location {
+	margin-bottom: 10px;
+	width: 90%; 
+}
+#content_input {
+	width: 99%;
+	height:870px;
+	display: block;
+}
+#alias_input {
+	width: 98%;
+	height: 300px;
+	margin: 10px 0px;
+	display: block;
+}
+#upload_alias {
+	margin: 10px 0px;
+	padding: 3px;
+}
+#current_alias {
+	border: 1px solid black;
+	width: 100%;
+	height: 30px;
+	color: red;
+}
+#outlineButton {
+	margin: 10px 0px;
+	padding: 3px;
+}
+#aliasList {
+	margin: 10px 0px;
+}
+#alias_show {
+	margin: 10px 0px;
+	padding: 10px 0px;
+}
+#output {
+	margin: 10px 0px;
+	padding: 3px;
+}
+#output_container {
+	width: 98%;
+	height: 300px;
+	margin: 10px 0px;
+	display: block;
+}
